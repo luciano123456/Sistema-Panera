3ae2f48fa9f3f525fced83b43e64cca54eba46a8
Luciano <luciano_5258@hotmail.com>
2025-01-25 03:28:16 -0300

    Se desarrolla productos tipos

---
+++ SistemaPanera.BLL/Service/IProductosTipoService.cs
@@ -0,0 +1,16 @@
+﻿using SistemaPanera.Models;
+
+namespace SistemaPanera.BLL.Service
+{
+    public interface IProductosTiposervice
+    {
+        Task<bool> Eliminar(int id);
+        Task<bool> Actualizar(ProductosTipo model);
+        Task<bool> Insertar(ProductosTipo model);
+
+        Task<ProductosTipo> Obtener(int id);
+
+        Task<IQueryable<ProductosTipo>> ObtenerTodos();
+    }
+
+}
---
+++ SistemaPanera.DAL/Repository/IProductosTipoRepository.cs
@@ -0,0 +1,19 @@
+﻿using SistemaPanera.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPanera.DAL.Repository
+{
+    public interface IProductosTipoRepository<TEntityModel> where TEntityModel : class
+    {
+        Task<bool> Eliminar(int id);
+        Task<bool> Actualizar(ProductosTipo model);
+        Task<bool> Insertar(ProductosTipo model);
+        Task<ProductosTipo> Obtener(int id);
+        Task<IQueryable<ProductosTipo>> ObtenerTodos();
+    }
+}
